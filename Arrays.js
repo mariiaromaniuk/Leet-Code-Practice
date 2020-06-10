732f6162c85abea21e6382a4fb1fb9dbc7367b69
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-06-09 23:29:12 -0400

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -87,6 +87,37 @@ function isPalindrome(node) {
    in the array. Your algorithm's runtime complexity must be in the order of O(log n).
 */
 
+// OPTION 1
+var search = function(nums, target) {
+  let mid, left = 0;
+  let right = nums.length - 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i+1])
+      mid = i;
+  }
+    
+  if (target === nums[mid])
+    return mid;
+
+  if (target >= nums[left] && target < nums[mid])
+    right = mid - 1;
+  else if (target >= nums[mid + 1] && target <= nums[right])
+    left = mid + 1;
+
+  while (left <= right) {
+    mid = Math.floor((right + left) / 2);
+    if (target === nums[mid])
+      return mid;
+    else if (target < nums[mid])
+      right = mid - 1;
+    else if (target > nums[mid])
+      left = mid + 1;
+  }
+  return -1;
+};
+
+// OPTION 2
 var search = function(nums, target) {
     let left = 0;
     let right = nums.length-1;
