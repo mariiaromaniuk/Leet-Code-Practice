67a9832b4ba62ed8b501d375d413ce3b504fdfe7
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-29 13:33:49 -0500

    Create Greater_Tree.js

---
+++ Greater_Tree.js
@@ -0,0 +1,30 @@
+/*
+Given a Binary Search Tree (BST), convert it to a Greater Tree
+Every key of the original BST is changed to
+the original key plus sum of all keys greater than the original key in BST.
+Example:
+Input: The root of a Binary Search Tree like this:
+              5
+             / \
+           2    13
+Output: The root of a Greater Tree like this:
+             18
+            /  \
+          20    13
+*/
+
+// Recursion backtrack traverse
+function convertBST(root){
+  var sum = 0;
+  traverse(root, 0);
+  return root;
+
+  function traverse(node){
+    if (node === null) return;
+    traverse(node.right, sum);
+    node.val += sum;
+    sum = node.val;
+    traverse(node.left);
+  }
+}
+
