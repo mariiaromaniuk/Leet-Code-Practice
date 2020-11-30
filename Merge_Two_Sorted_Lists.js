a378113cb2b72bfb427b6b87b37962e7b45822a5
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-29 19:21:01 -0500

    Create Merge_Two_Sorted_Lists.js

---
+++ Merge_Two_Sorted_Lists.js
@@ -0,0 +1,36 @@
+// Merge two sorted linked lists and return it as a new sorted list. 
+// The new list should be made by splicing together the nodes of the first two lists.
+
+// OPTION 1 --> Recursive
+// Time: O(min(N,M)), Space: O(1)
+function mergeTwoLists(l1, l2){
+  if(!l1 || !l2) return l1 || l2;
+  if (l1.val < l2.val){
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+  // Given that both lists are null-terminated, 
+  // the recursion will eventually terminate
+}
+ 
+// OPTION 2 --> Iterative
+// Time: O(min(N,M)), Space: O(min(N,M))
+function mergeTwoLists(l1, l2){
+  const head = new Node(null);
+  let curr = head;
+  while (l1 && l2){
+    if (l1.val < l2.val){
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = l1 || l2;
+  return head.next;
+}
