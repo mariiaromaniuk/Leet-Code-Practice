a176e23fc4d15f9b29aab593c01e463263e0cc2c
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-30 00:27:13 -0500

    Create Sum_Nested_Array.js

---
+++ Sum_Nested_Array.js
@@ -0,0 +1,25 @@
+// Write a function that sums all numbers in an array that can have nested sub-arrays.
+
+/* 
+  simplier:
+  return arr.reduce((a, b) => {
+    return a + sumNestedArray(b);
+  }, 0);
+*/
+function sumNestedArray(arr){
+  return arr.reduce((a, b) => {
+    if( !Array.isArray(b) ) return a + b;
+    return a + sumNestedArray(b);
+  }, 0);
+}
+
+// no iteratoin: sum(input, input.length-1)
+// simplier: return sumNestedArray2(arr, i - 1) + current;
+function sumNestedArray(arr, i){
+  if (i < 0) return 0;
+  let current = arr[i];
+  if (Array.isArray(current)){
+    current = sumNestedArray2(current, current.length - 1);
+  }
+  return sumNestedArray2(arr, i - 1) + current;
+}
