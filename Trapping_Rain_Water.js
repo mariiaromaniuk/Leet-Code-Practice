ea22c6d8e019ba7116f3ec44ee359ffdf1352006
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-01 14:52:07 -0500

    Update Trapping_Rain_Water.js

--- Trapping_Rain_Water.js
+++ Trapping_Rain_Water.js
@@ -1,5 +1,9 @@
 /*
  Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+ 
+ Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ Output: 6
+ The elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water are being trapped.
 */
 // OPTION 2 --> Two Pointers
 // Time: O(n), Space: O(1)
