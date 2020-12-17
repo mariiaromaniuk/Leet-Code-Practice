43b95af8b924168ee010a3e6635de3b6df84f2f2
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-17 13:58:38 -0500

    Update Conflicting_Appointments.js

--- Intervals/Conflicting_Appointments.js
+++ Intervals/Conflicting_Appointments.js
@@ -29,3 +29,21 @@ function can_attend_all_appointments(intervals) {
   return true;
 }
 
+// Test
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+  new Interval(1, 4),
+  new Interval(2, 5),
+  new Interval(7, 9),
+])}`);
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+  new Interval(6, 7),
+  new Interval(2, 4),
+  new Interval(8, 12),
+])}`);
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+  new Interval(4, 5),
+  new Interval(2, 3),
+  new Interval(3, 6),
+])}`);
