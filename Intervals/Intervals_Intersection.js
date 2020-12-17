d66343480cd1bc4f96672b1734ca675b0690a5ee
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-17 13:56:46 -0500

    Create Intervals_Intersection.js

---
+++ Intervals/Intervals_Intersection.js
@@ -0,0 +1,44 @@
+// Given two lists of intervals, find the intersection of these two lists. 
+// Each list consists of disjoint intervals sorted on their start time.
+// Input: arr1=[[1, 3], [5, 6], [7, 9]], arr2=[[2, 3], [5, 7]]
+// Output: [2, 3], [5, 6], [7, 7] -> Common intervals between the two lists.
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+
+function merge(intervals_a, intervals_b) {
+  let result = [],
+    i = 0,
+    j = 0;
+
+  while (i < intervals_a.length && j < intervals_b.length) {
+    // check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
+    a_overlaps_b = intervals_a[i].start >= intervals_b[j].start && intervals_a[i].start <= intervals_b[j].end;
+
+    // check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
+    b_overlaps_a = intervals_b[j].start >= intervals_a[i].start && intervals_b[j].start <= intervals_a[i].end;
+
+    // store the the intersection part
+    if (a_overlaps_b || b_overlaps_a) {
+      result.push(new Interval(Math.max(intervals_a[i].start, intervals_b[j].start),
+        Math.min(intervals_a[i].end, intervals_b[j].end)));
+    }
+    // move next from the interval which is finishing first
+    if (intervals_a[i].end < intervals_b[j].end) {
+      i += 1;
+    } else {
+      j += 1;
+    }
+  }
+
+  return result;
+}
+
