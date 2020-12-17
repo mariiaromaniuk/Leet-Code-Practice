fa505ae8f309103d1d3b5e868f6f9c276fd02363
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-17 13:57:03 -0500

    Update Intervals_Intersection.js

--- Intervals/Intervals_Intersection.js
+++ Intervals/Intervals_Intersection.js
@@ -42,3 +42,17 @@ function merge(intervals_a, intervals_b) {
   return result;
 }
 
+// Test
+process.stdout.write('Intervals Intersection: ');
+let result = merge([new Interval(1, 3), new Interval(5, 6), new Interval(7, 9)], [new Interval(2, 3), new Interval(5, 7)]);
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+console.log();
+
+process.stdout.write('Intervals Intersection: ');
+result = merge([new Interval(1, 3), new Interval(5, 7), new Interval(9, 12)], [new Interval(5, 10)]);
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+console.log();
