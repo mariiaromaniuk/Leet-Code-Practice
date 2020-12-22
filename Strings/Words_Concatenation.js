848e05a772add7fc7b9bf7f84cee143e589f0853
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-21 20:38:09 -0500

    Update Words_Concatenation.js

--- Strings/Words_Concatenation.js
+++ Strings/Words_Concatenation.js
@@ -55,3 +55,14 @@ function find_word_concatenation(str, words){
 // Test
 console.log(find_word_concatenation('catfoxcat', ['cat', 'fox']));
 console.log(find_word_concatenation('catcatfoxfox', ['cat', 'fox']));
+
+/*
+ Time: O(n * m * len) 
+   ‘n’ is the number of characters in the given string, 
+   ‘m’ is the total number of words, 
+   ‘len’ is the length of a word.
+ Space: O(m) 
+   at most, we will be storing all the words in the two HashMaps. 
+   the worst case, we also need O(n) space for the resulting list. 
+   So, the overall space complexity of the algorithm will be O(n + m).
+*/
