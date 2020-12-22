4f670d7a39f886b3e0c057bc7d3ac7acd373fd20
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-21 20:27:53 -0500

    Update Max_CPU_Load.js

--- Intervals/Max_CPU_Load.js
+++ Intervals/Max_CPU_Load.js
@@ -49,3 +49,8 @@ function maxCpuLoad(jobs){
   }
   return maxCPULoad;
 }
+
+// Test
+console.log(maxCpuLoad([new Job(1,4,3), new Job(2,5,4), new Job(7,9,6)]));
+console.log(maxCpuLoad([new Job(6,7,10), new Job(2,4,11), new Job(8,12,15)]));
+console.log(maxCpuLoad([new Job(1,4,2), new Job(2,4,1), new Job(3,6,5)]));
