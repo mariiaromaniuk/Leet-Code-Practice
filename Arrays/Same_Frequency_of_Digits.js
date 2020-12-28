83e4d262bef97ff04de9e493ddae7c0bae2ff88b
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-27 21:48:44 -0500

    Update Same_Frequency_of_Digits.js

--- Arrays/Same_Frequency_of_Digits.js
+++ Arrays/Same_Frequency_of_Digits.js
@@ -46,3 +46,8 @@ function sameFrequency(int1, int2){
   return true;
 }
 
+// Test
+console.log(sameFrequency(182, 281)) // true;
+console.log(sameFrequency(34, 14)) // false;
+console.log(sameFrequency(3589578, 5879385)) // true;
+console.log(sameFrequency(22, 222)) // false;
