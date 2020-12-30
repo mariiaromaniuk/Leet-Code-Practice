ee9e0248c3e14b88c943038ef45a67f351ecb67c
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-30 17:12:51 -0500

    Update Reduce_Path.js

--- Arrays/Reduce_Path.js
+++ Arrays/Reduce_Path.js
@@ -5,6 +5,8 @@ function directionReduce(arr){
     'SOUTH': 'NORTH', 
     'WEST': 'EAST'
   };
+  // reduce() method executes a reducer function (that I provided) 
+  // on each element of the array, resulting in single output value
   return arr.reduce(function(direction, nextDirection){
     direction[direction.length - 1] === opposite[nextDirection] ? direction.pop(): direction.push(nextDirection);
     return direction;
