00b9aeadce55cc27a6499e3574dc2b3ab95f8ef6
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-30 17:16:15 -0500

    Update Reduce_Path.js

--- Arrays/Reduce_Path.js
+++ Arrays/Reduce_Path.js
@@ -7,6 +7,8 @@ function directionReduce(arr){
   };
   // reduce() method executes a reducer function (that I provided) 
   // on each element of the array, resulting in single output value
+  // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  // arr.reduce(reducer)
   return arr.reduce(function(direction, nextDirection){
     direction[direction.length - 1] === opposite[nextDirection] ? direction.pop(): direction.push(nextDirection);
     return direction;
