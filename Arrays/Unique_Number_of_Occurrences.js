c8cf5872344809f2eaad04be5dbc38b69382853f
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-31 23:07:21 -0500

    Update Unique_Number_of_Occurrences.js

--- Arrays/Unique_Number_of_Occurrences.js
+++ Arrays/Unique_Number_of_Occurrences.js
@@ -1,1 +1,4 @@
 // Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+// Example: [1,2,2,1,1,3] --> true
+// The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
