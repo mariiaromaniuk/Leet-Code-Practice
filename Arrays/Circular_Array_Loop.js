c10f53669f3fc8e95b13f8b1fac3bd9d009b5505
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-31 23:29:10 -0500

    Update Circular_Array_Loop.js

--- Arrays/Circular_Array_Loop.js
+++ Arrays/Circular_Array_Loop.js
@@ -14,3 +14,24 @@
  The movement from index 1 -> 1 -> 1 ... is not a cycle, because the cycle's length is 1. By definition the cycle's length must be greater than 1.
 */
   
+var circularArrayLoop = (nums) => {
+    for (let i = 0; i < nums.length; ++i) {
+        let cur = i, count = 0, dir = 0, prev = i;
+        while (nums[cur] <= 1000) {
+            prev = cur;
+            let t = (cur + nums[cur]) % nums.length;
+            if (nums[cur] < 0) {
+                cur = t < 0 ? (nums.length + t) : t;
+                dir = (dir === 0 || dir === 1) ? 1 : 3; 
+            } else {
+                cur = t;
+                dir = (dir === 0 || dir === 2) ? 2 : 3;
+            }
+            if (dir === 3) break;
+            nums[prev] = 1001 + i;
+            if (cur === prev) break;
+        }
+        if (prev !== cur && nums[cur] === (1001 + i) && dir !== 3) return true;
+    }
+    return false;
+};
