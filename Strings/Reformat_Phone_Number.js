4fd602166f89930d19efbe33f069117229260506
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-01 22:21:55 -0500

    Update Reformat_Phone_Number.js

--- Strings/Reformat_Phone_Number.js
+++ Strings/Reformat_Phone_Number.js
@@ -9,6 +9,39 @@
  
  The blocks are then joined by dashes. Notice that the reformatting process should never produce any blocks 
  of length 1 and produce at most two blocks of length 2. Return the phone number after formatting.
+ 
+ Example 1:
+ Input: number = "1-23-45 6"
+ Output: "123-456"
+ Explanation: The digits are "123456".
+ Step 1: There are more than 4 digits, so group the next 3 digits. The 1st block is "123".
+ Step 2: There are 3 digits remaining, so put them in a single block of length 3. The 2nd block is "456".
+ Joining the blocks gives "123-456".
+ 
+ Example 2:
+ Input: number = "123 4-567"
+ Output: "123-45-67"
+ Explanation: The digits are "1234567".
+ Step 1: There are more than 4 digits, so group the next 3 digits. The 1st block is "123".
+ Step 2: There are 4 digits left, so split them into two blocks of length 2. The blocks are "45" and "67".
+ Joining the blocks gives "123-45-67".
+
+ Example 3:
+ Input: number = "123 4-5678"
+ Output: "123-456-78"
+ Explanation: The digits are "12345678".
+ Step 1: The 1st block is "123".
+ Step 2: The 2nd block is "456".
+ Step 3: There are 2 digits left, so put them in a single block of length 2. The 3rd block is "78".
+ Joining the blocks gives "123-456-78".
+
+ Example 4:
+ Input: number = "12"
+ Output: "12"
+
+ Example 5:
+ Input: number = "--17-5 229 35-39475 "
+ Output: "175-229-353-94-75"
 */
 
 // OPTION 1
