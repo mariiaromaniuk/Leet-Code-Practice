e730416a3efc8ecb4df3fdeb2b2c7e44fee6595a
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-01 22:12:22 -0500

    Update Maximum_Erasure_Value.js

--- Arrays/Maximum_Erasure_Value.js
+++ Arrays/Maximum_Erasure_Value.js
@@ -5,3 +5,21 @@
  An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is 
  equal to a[l],a[l+1],...,a[r] for some (l,r).
  */
+
+var maximumUniqueSubarray = function(nums) {
+    let l = 0, r = 0, max = 0,hashSet=new Set(),sum = 0;
+    while (r<nums.length) {
+       if (!hashSet.has(nums[r])) {
+           hashSet.add(nums[r]);
+           sum += nums[r];
+           max = Math.max(max, sum);
+           r++;
+           
+       } else {
+           hashSet.delete(nums[l]);
+           sum -= nums[l];
+           l++; 
+       }                          
+    }
+return max;    
+};
