c8ddca75b6e4bef5896c0962677b20e63220a6c2
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-01 22:14:03 -0500

    Update Maximum_Erasure_Value.js

--- Arrays/Maximum_Erasure_Value.js
+++ Arrays/Maximum_Erasure_Value.js
@@ -23,3 +23,14 @@ function maximumUniqueSubarray(nums) {
   }
   return max;    
 }
+
+// OPTION 2
+var maximumUniqueSubarray = function(N) {
+    let a = new Int8Array(10001), sum = max = 0, len = N.length
+    for (let l = 0, r = 0; r < len; r++) {
+        a[N[r]]++, sum += N[r]
+        while (a[N[r]] > 1) a[N[l]]--, sum -= N[l++]
+        max = sum > max ? sum : max
+    }
+    return max
+};
