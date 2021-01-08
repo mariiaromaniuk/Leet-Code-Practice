082ab1b6f01b39bfbb9c2d11548a6ea7f6b7ce31
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-07 20:57:34 -0500

    Update Arithmetic_Subarrays.js

--- Arrays/Arithmetic_Subarrays.js
+++ Arrays/Arithmetic_Subarrays.js
@@ -71,3 +71,7 @@ function checkArithmeticSubarrays(nums, l, r) {
   }
   return boolArr;
 }
+
+// Test
+console.log(checkArithmeticSubarrays([4,6,5,9,3,7], [0,0,2], 2,3,5])); // [true,false,true]
+console.log(checkArithmeticSubarrays([-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], [0,1,6,4,8,7], [4,4,9,7,9,10])); // [false,true,false,false,true,true]
