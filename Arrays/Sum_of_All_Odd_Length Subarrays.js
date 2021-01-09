642ce38dc8ebbf4e90bd5ae162ef1dda430c5684
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-08 22:45:17 -0500

    Update Sum_of_All_Odd_Length Subarrays.js

--- Arrays/Sum_of_All_Odd_Length Subarrays.js
+++ Arrays/Sum_of_All_Odd_Length Subarrays.js
@@ -76,3 +76,8 @@ function sumOddLengthSubarrays(arr){
   }
   return sum;
 }
+
+// Test
+console.log(sumOddLengthSubarrays([1,4,2,5,3])); // 58
+console.log(sumOddLengthSubarrays([1,2])); // 3
+console.log(sumOddLengthSubarrays([10,11,12])); // 66
