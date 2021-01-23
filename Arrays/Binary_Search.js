ed18cc7b046e0473acc2ea60793b885859f57c48
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-22 21:54:48 -0500

    Update Binary_Search.js

--- Arrays/Binary_Search.js
+++ Arrays/Binary_Search.js
@@ -1,23 +1,23 @@
 /*
-   Given a sorted (in ascending order) integer array nums of n elements and a target value, 
-   write a function to search target in nums. If target exists, then return its index, 
-   otherwise return -1.
-   Time Complexity: O(logn), Space Complexity: O(1).
+ Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+ write a function to search target in nums. If target exists, then return its index, 
+ otherwise return -1.
+ Time Complexity: O(logn), Space Complexity: O(1).
 */
 
-var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length-1;
-    let mid;
+function search(nums, target){
+  let left = 0;
+  let right = nums.length-1;
+  let mid;
     
-    while (left <= right) {
-        mid = Math.ceil((right + left) / 2);
-        if (target === nums[mid])
-            return mid;
-        else if (target < nums[mid])
-            right = mid - 1;
-        else if (target > nums[mid])
-            left = mid + 1;
-    }
-    return -1;
-};
+  while (left <= right){
+    mid = Math.ceil((right + left) / 2);
+    if (target === nums[mid])
+      return mid;
+    else if (target < nums[mid])
+      right = mid - 1;
+    else if (target > nums[mid])
+      left = mid + 1;
+  }
+  return -1;
+}
