fbbf4f88e4d65bf3f8fcbc1867f753b2fce7af9a
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-25 23:51:25 -0500

    Update Diagonal_Traverse.js

--- Arrays/Diagonal_Traverse.js
+++ Arrays/Diagonal_Traverse.js
@@ -10,3 +10,21 @@
 
  Output:  [1,2,4,7,5,3,6,8,9]
 */
+
+function findDiagonalOrder(matrix){
+  if (matrix.length===0 || matrix[0].length==0) return [];
+  let rows = matrix.length;
+  let columns = matrix[0].length;
+  let results = new Array(rows+columns-1);
+  
+  for (let i = 0; i < rows; i++){
+    for (let j = 0; j < columns; j++){
+      if (!results[i+j]) results[i+j] = [];
+      results[i+j].push(matrix[i][j]);
+    }
+  }
+  for (let i = 0; i < (rows + columns-1); i = i+2){
+    if (i%2 === 0) results[i] = results[i].reverse();
+  }
+  return results.flat()
+}
