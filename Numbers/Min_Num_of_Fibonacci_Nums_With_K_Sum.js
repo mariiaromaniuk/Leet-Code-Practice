8c582602a56b87f80876565fc27fd8ce8b9c6469
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-28 22:15:29 -0500

    Update Min_Num_of_Fibonacci_Nums_With_K_Sum.js

--- Numbers/Min_Num_of_Fibonacci_Nums_With_K_Sum.js
+++ Numbers/Min_Num_of_Fibonacci_Nums_With_K_Sum.js
@@ -22,3 +22,19 @@
  Output: 3 
  Explanation: For k = 19 we can use 1 + 5 + 13 = 19.
 */
+
+function findMinFibonacciNumbers(k) {
+  let sequence = [1, 1], sum = sequence[0] + sequence[1];
+  let i = 2;
+  while (sum <= k){
+    sequence.push(sum);
+    i++;
+    sum = sequence[i-1]+sequence[i-2];
+  }
+  let j = sequence.length-1, res = 0;
+  while (k) {
+    if (k >= sequence[j]) k -= sequence[j], res++;
+    j--;
+  }
+  return res;
+}
