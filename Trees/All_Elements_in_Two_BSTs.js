e2b1413bc1e9312e255f49c7b0970d99a52bb6e1
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2021-01-29 23:55:21 -0500

    Update All_Elements_in_Two_BSTs.js

--- Trees/All_Elements_in_Two_BSTs.js
+++ Trees/All_Elements_in_Two_BSTs.js
@@ -18,3 +18,27 @@
  Input: root1 = [0,-10,10], root2 = []
  Output: [-10,0,10]
 */
+
+// To convert a binary search tree to an ordered array, we use inorder traversal.
+// To merge 2 sorted queue, we pick the top of both, do the comparison, and refresh the top of the bigger one.
+const getAllElements = (root1, root2) => {
+  const arr1 = [];
+  const arr2 = [];
+  traversal(root1, arr1);
+  traversal(root2, arr2);
+  const ret = [];
+  let idx1 = idx2 = 0;
+  while (idx1 < arr1.length && idx2 < arr2.length) {
+    arr1[idx1] < arr2[idx2] ? ret.push(arr1[idx1++]) : ret.push(arr2[idx2++]);
+  }
+  while (idx1 < arr1.length) ret.push(arr1[idx1++]);
+  while (idx2 < arr2.length) ret.push(arr2[idx2++]);
+  return ret;
+
+  function traversal(node, arr) {
+    if (!node) return;
+    traversal(node.left, arr);
+    arr.push(node.val);
+    traversal(node.right, arr);
+  }
+};
